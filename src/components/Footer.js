6fc7677079e90a253376129318559dfa156195dd
Anujmishra2005 <anujmishraan2005@gmail.com>
2024-07-10 22:17:14 +0530

    New Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer className='mt-8 w-full py-8 px-6 bg-black text-white'>
+    <p className='text-center'>Footer <a>Jujutsu Kaisen</a> 2024</p>
+  </footer>
+}
+
+export default Footer
